3d4152e916e8ff089221852278d416210b644b33
Joel Kuehne <joelckuehne@gmail.com>
2026-05-28 15:58:57 -0500

    I'm about to do something very evil

--- ref/src/rvvi.cc
+++ ref/src/rvvi.cc
@@ -1,30 +1,44 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string.h>
+
 #include "rvvi.h"
-#include <riscv/sim.h>
-#include <fesvr/elfloader.h>
 
 #define MEM_BASE 0x0
 #define MEM_SIZE 0x8000
 #define ENTRYPOINT 0x0
+#define TOSTR(x) #x
 
 struct DutState {
+	// TODO: If this is ever multi core, need to extend this state.
 	uint32_t pc;
 	uint32_t insn;
 	uint32_t gpr[32];
-	uint32_t gpr_written[32];
+	bool gpr_written[32];
 } dutState;
 
-cfg_t *spike_cfg = nullptr;
-sim_t *spike_sim = nullptr;
+pid_t spike_pid;
+int host_to_spike[2];
+int spike_to_host[2];
 
-static std::vector<std::pair<reg_t, abstract_mem_t*>> make_mems(const std::vector<mem_cfg_t> &layout)
+int spikeStep()
 {
-  std::vector<std::pair<reg_t, abstract_mem_t*>> mems;
-  mems.reserve(layout.size());
-  for (const auto &cfg : layout) {
-    mems.push_back(std::make_pair(cfg.get_base(), new mem_t(cfg.get_size())));
-  }
-  return mems;
+	const char STEP_CMD[] = "r 1\n";
+	char buffer[256];
+
+	if (write(host_to_spike[1], STEP_CMD, strlen(STEP_CMD))) {
+		perror("write");
+		return -1;
+	}
+}
+
+uint32_t spikeGetGpr(int regId)
+{
+	char buf[256];
+	sprintf(buf, "reg 0 %d\n", regId);
 }
 
 extern "C" {
@@ -36,83 +50,112 @@ bool_t rvviVersionCheck(uint32_t version)
 
 bool_t rvviRefInit(const char *programPath)
 {
-    // TODO: Remove
-    std::cerr << "Test 0" << std::endl;
+	bool_t result = RVVI_TRUE;
 
-	// Initialize the configuration for the simulator
-	cfg_t *cfg = new cfg_t();
-    cfg->initrd_bounds      = std::make_pair(0, 0); // Do not load a filesystem
-    cfg->bootargs           = nullptr;              // No bootargs
-	cfg->isa                = "rv32i";              // Configure for rv32i
-	cfg->priv               = "priv_spec_1p11";     // Specify grabage priv_spec
-    cfg->endianness         = endianness_little;    // Little endian is risc-v default
-    cfg->pmpregions         = 0;                    // No protected memory regions
-    cfg->pmpgranularity     = 0;                    // No protected memory regions
-    cfg->mem_layout         = std::vector<mem_cfg_t>({mem_cfg_t(MEM_BASE, MEM_SIZE)});
-    cfg->start_pc.set_global(ENTRYPOINT);           // Start from addr 0x0
-    cfg->hartids            = std::vector<size_t>({0});
-    cfg->explicit_hartids   = true;                 // Use the specified hartids
-    cfg->real_time_clint    = false;                // Use the simulation clock for interrupts
-    cfg->trigger_count      = 0;                    // No software breakpoints
-    cfg->cache_blocksz      = 64;                   // Default standard cache line sizing
-    cfg->external_simulator = std::nullopt;         // No external simulator
+	// TODO: Figure out where this should go.
+	for (int i = 0; i < 32; i++)
+		dutState.gpr_written[i] = false;
 
-    // TODO: Remove
-    std::cerr << "Test 1" << std::endl;
-
-    // Set up parameters for the simulator
-    bool halted = false;                // CPU is not halted
-    std::vector<std::pair<reg_t, abstract_mem_t*>> mems = make_mems(cfg->mem_layout);
-    std::vector<device_factory_sargs_t> plugin_device_factories;
-    bool dtb_discovery = false;         // No device tree binary
-    std::vector<std::string> htif_args; // Do not automaticaly load an elf file
-    debug_module_config_t dm_config;    // No debug module
-    const char *log_path = nullptr;     // No loggin
-    bool dtb_enabled = false;           // No device tree binary
-    const char *dtb_file = nullptr;     // No device tree binary
-    bool socket_enabled = false;        // No socket
-    FILE *cmd_file = nullptr;           // No command file
-    std::optional<unsigned long long> instruction_limit = std::nullopt;
-
-    // TODO: Remove
-    std::cerr << "Test 2" << std::endl;
-
-    // Initialize the simulator itself
-    spike_sim = new sim_t(
-    	cfg, halted, mems, plugin_device_factories, dtb_discovery,
-    	htif_args, dm_config, log_path, dtb_enabled, dtb_file,
-    	socket_enabled, cmd_file, instruction_limit
-    );
-
-    // TODO: Remove
-    std::cerr << "Test 3" << std::endl;
-
-	// Load the elf file
-	if (programPath != nullptr) {
-		std::ifstream file(programPath, std::ios::binary | std::ios::ate);
-		reg_t entry_point = 0;
-
-    // TODO: Remove
-    std::cerr << "Test 4" << std::endl;
-
-		// Load the elf from the file
-		try {
-			load_elf(programPath, &(spike_sim->memif()), &entry_point, 0);
-		} catch (const std::exception& e) {
-			return RVVI_FALSE;
-		}
-
-		// Set the entrypoint for the core
-		spike_sim->get_core(0)->get_state()->pc = entry_point;
-		if (file.is_open()) {
-			std::streamsize size = file.tellg();
-		}
+	// Create pipes to communicate with the spike simulator
+	if (pipe(host_to_spike)) {
+		perror("pipe");
+		result = RVVI_FALSE;
+		goto out;
 	}
 
-    // TODO: Remove
-    std::cerr << "Test 5" << std::endl;
+	if (pipe(spike_to_host)) {
+		perror("pipe");
+		result = RVVI_FALSE;
+		goto out;
+	}
 
+	// Fork and exec the spike simulator
+	if ((spike_pid = fork()) == -1) {
+		perror("fork");
+		result = RVVI_FALSE;
+		goto err_close;
+	}
+
+	if (spike_pid == 0) {
+		// Close the unneded ends of the pipe
+		close(host_to_spike[1]);
+		close(spike_to_host[0]);
+
+		// Move the pipe to STDIN and STDOUT
+		if (dup2(host_to_spike[0], STDIN_FILENO) != 0) {
+			perror("dup2");
+			result = RVVI_FALSE;
+			_exit(1);
+		}
+
+		if (dup2(spike_to_host[1], STDOUT_FILENO) != 0) {
+			perror("dup2");
+			result = RVVI_FALSE;
+			_exit(1);
+		}
+
+		// Close the remaining ends of the pipe
+		close(host_to_spike[1]);
+		close(spike_to_host[0]);
+
+		// Run the spike simulator
+		execlp("spike", "spike", "-m" TOSTR(MEM_BASE) ":" TOSTR(MEM_SIZE) "0x4000", "--isa=rv32i",
+		       "--disable-dtb", "-d", "--pc=" TOSTR(MEM_BASE), programPath, NULL);
+		perror("execlp");
+		_exit(1);
+	}
+
+	close(host_to_spike[0]);
+	host_to_spike[0] = 0;
+	close(spike_to_host[1]);
+	spike_to_host[1] = 1;
+
+err_kill:
+	kill(spike_pid, SIGKILL);
+	waitpid(spike_pid, NULL, 0);
+
+err_close:
+	if (host_to_spike[0] != 0)
+		close(host_to_spike[0]);
+	if (host_to_spike[1] != 0)
+		close(host_to_spike[1]);
+	if (spike_to_host[0] != 0)
+		close(spike_to_host[0]);
+	if (spike_to_host[1] != 0)
+		close(spike_to_host[1]);
+
+out:
+	return result;
+}
+
+bool_t rvviRefShutdown()
+{
+	// Is there really any reason to be nice?
+	kill(spike_pid, SIGKILL);
+	waitpid(spike_pid, NULL, 0);
 	return RVVI_TRUE;
+}
+
+void rvviDutGprSet(uint32_t hartId, uint32_t gprIndex, uint64_t value)
+{
+	dutState.gpr[gprIndex] = value;
+	dutState.gpr_written[gprIndex] = true;
+}
+
+void rvviDutRetire(uint32_t hartId, uint64_t dutPc, uint64_t dutInsBin, bool_t debugMode)
+{
+	// Do nothing
+}
+
+bool_t rvviRefEventStep(uint32_t hartId)
+{
+	return spikeStep() == 0 ? RVVI_FALSE : RVVI_TRUE;
+}
+
+bool_t rvviRefGprsCompare(uint32_t hartId)
+{
+	for (int i = 0; i < 32; i++) {
+	}
 }
 
 }
